7e5ad7941cdc7fb0e7217db6aa2040d673b6a243
Matthew Hanson <matt.a.hanson@gmail.com>
2016-11-30 11:45:03 -0500

    no need for all lib64 python libs

--- deploy/package.sh
+++ deploy/package.sh
@@ -3,23 +3,23 @@
 DEPLOY_DIR=deploy
 
 mkdir -p $DEPLOY_DIR/lib/python2.7
-mkdir -p $DEPLOY_DIR/lib64/python2.7
 
 # package libraries
-cp -L /usr/lib64/libagg.so.2 $DEPLOY_DIR/lib64/
-cp -L /usr/lib64/libpotrace.so.0 $DEPLOY_DIR/lib64/
+cp -L /usr/lib64/libagg.so.2 $DEPLOY_DIR/lib/
+cp -L /usr/lib64/libpotrace.so.0 $DEPLOY_DIR/lib/
 cp /usr/local/lib/libgdal.so.1 $DEPLOY_DIR/lib/
 
 # package app and python libs
 pip install .
 # rsync is the best way
 rsync -ax /usr/lib/python2.7/site-packages/ $DEPLOY_DIR/lib/python2.7/site-packages/ --exclude-from $DEPLOY_DIR/excluded_packages
-rsync -ax /usr/lib64/python2.7/site-packages/ $DEPLOY_DIR/lib64/python2.7/site-packages/ --exclude-from $DEPLOY_DIR/excluded_packages
+# gdal seems to be in it's own dir, so we'll link it
+rsync -ax /usr/lib64/python2.7/site-packages/GDAL-1.11.5-py2.7-linux-x86_64.egg/osgeo $DEPLOY_DIR/lib/python2.7/site-packages/
+rsync -ax /usr/lib64/python2.7/site-packages/potrace $DEPLOY_DIR/lib/python2.7/site-packages/
+rsync -ax /usr/lib64/python2.7/site-packages/gippy $DEPLOY_DIR/lib/python2.7/site-packages/
 # but if running docker on mac, comment out above two lines and uncomment these two lines
 #cp -R /usr/lib $DEPLOY_DIR/lib
 #cp -R /usr/lib64 $DEPLOY_DIR/lib64
-# gdal seems to be in it's own dir, so we'll link it
-ln -s GDAL-1.11.5-py2.7-linux-x86_64.egg/osgeo $DEPLOY_DIR/lib64/python2.7/site-packages/osgeo
 # create link to CLI
 ln -s lib/python2.7/site-packages/bfalg_ndwi/ndwi.py $DEPLOY_DIR/bfalg-ndwi.py
 
