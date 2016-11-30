04b23b965cbfe601b54212bcadae46c03baa2d4f
Matthew Hanson <matt.a.hanson@gmail.com>
2016-11-30 01:55:32 -0500

    minimized package

--- deploy/package.sh
+++ deploy/package.sh
@@ -1,36 +1,25 @@
 #!/bin/bash
 
-DEPLOY_DIR=app/deploy
+DEPLOY_DIR=deploy
+
+mkdir -p $DEPLOY_DIR/lib/python2.7
+mkdir -p $DEPLOY_DIR/lib64/python2.7
 
 # package libraries
-#mkdir -p $DEPLOY_DIR/lib/python2.7
-#mkdir -p $DEPLOY_DIR/lib64/python2.7
-
-#cp /usr/lib64/libhdf5.so.8 $DEPLOY_DIR/lib64/
-#cp /usr/lib64/libnetcdf.so.7 $DEPLOY_DIR/lib64/
-#cp /usr/lib64/libjasper.so.1 $DEPLOY_DIR/lib64/
-#cp /usr/lib64/libopenjp2.so.7 $DEPLOY_DIR/lib64/
-#cp /usr/lib64/libxerces-c-3.1.so $DEPLOY_DIR/lib64/
-#cp /usr/lib64/libodbcinst.so.2 $DEPLOY_DIR/lib64/
-#cp /usr/lib64/libodbc.so.2 $DEPLOY_DIR/lib64/
-#cp /usr/lib64/libwebp.so.4 $DEPLOY_DIR/lib64/
-#cp /usr/lib64/libfreexl.so.1 $DEPLOY_DIR/lib64/
-#cp /usr/lib64/libpoppler.so.46 $DEPLOY_DIR/lib64/
-#cp /usr/lib64/libarmadillo.so.4 $DEPLOY_DIR/lib64/
-#cp /usr/lib64/libgeos_c.so.1 $DEPLOY_DIR/lib64/
-#cp /usr/lib64/libproj.so.0 $DEPLOY_DIR/lib64/
-#cp /usr/lib64/libgdal.so.1 $DEPLOY_DIR/lib64/
+cp -L /usr/lib64/libagg.so.2 $DEPLOY_DIR/lib64/
+cp -L /usr/lib64/libpotrace.so.0 $DEPLOY_DIR/lib64/
+cp /usr/local/lib/libgdal.so.1 $DEPLOY_DIR/lib/
 
 
 # package app and python libs
-pip install app/.
-#rsync -ax /usr/lib/python2.7/site-packages/ $DEPLOY_DIR/lib/python2.7/site-packages/ --exclude-from $DEPLOY_DIR/excluded_packages
-#rsync -ax /usr/lib64/python2.7/site-packages/ $DEPLOY_DIR/lib64/python2.7/site-packages/ --exclude-from $DEPLOY_DIR/excluded_packages
-rsync -ax /usr/lib/ $DEPLOY_DIR/lib/
-rsync -ax /usr/lib64/ $DEPLOY_DIR/lib64/
+pip install .
+rsync -ax /usr/lib/python2.7/site-packages/ $DEPLOY_DIR/lib/python2.7/site-packages/ --exclude-from $DEPLOY_DIR/excluded_packages
+rsync -ax /usr/lib64/python2.7/site-packages/ $DEPLOY_DIR/lib64/python2.7/site-packages/ --exclude-from $DEPLOY_DIR/excluded_packages
+# gdal seems to be in it's own dir, so we'll link it
+ln -s GDAL-1.11.5-py2.7-linux-x86_64.egg/osgeo $DEPLOY_DIR/lib64/python2.7/site-packages/osgeo
+# create link to CLI
 ln -s lib/python2.7/site-packages/bfalg_ndwi/ndwi.py $DEPLOY_DIR/bfalg-ndwi.py
-
 
 # zip up contents
 cd $DEPLOY_DIR
-#zip -ruq deploy.zip ./ -x deploy.zip excluded_packages package.sh
+zip -ruq ../deploy.zip ./ -x excluded_packages package.sh
